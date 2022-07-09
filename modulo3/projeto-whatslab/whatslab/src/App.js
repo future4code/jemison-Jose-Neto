472182b91adbe02f4c920d6fb6770b8f8e553cad
jose neto <jnetocdz@hotmail.com.br>
2022-07-08 22:23:59 -0300

    whatslab-incompleto

---
+++ modulo3/projeto-whatslab/whatslab/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <input type='text' name='remetente' id='remetente'/>
+      <input type='text' name='mensagem' id='mensagem'/>
+      <button>Enviar</button>
+    </main>
+
+
+
+)
+}
+
+export default App;
