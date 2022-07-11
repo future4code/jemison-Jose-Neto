1a79738130451eb6bcd8780d18843f7fc05b0bc0
jose neto <jnetocdz@hotmail.com.br>
2022-07-10 22:26:39 -0300

    whatslab

--- modulo3/projeto-whatslab/whatslab/src/App.js
+++ modulo3/projeto-whatslab/whatslab/src/App.js
@@ -1,13 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import{DivCentral,MsgInicial} from './style'
 
 function App() {
+
+  const[inputRemetente ,setInputRemetente] = useState('');
+  const[inputMensagem,setInputMensagem] = useState('');
+  const[primeiraMensagem,setPrimeiraMensagem] = useState ([{nome: "Chat", mensagem: "Olá, digite sua mensagem" }])
+
+  const handleInputRemetente = (e) =>{
+    setInputRemetente(e.target.value)
+  }
+
+  const handleInputMensagem = (e) =>{
+    setInputMensagem(e.target.value)
+  }
+
+  const addMensagem = (e) =>{
+    e.preventDefault();
+
+    setPrimeiraMensagem ([...primeiraMensagem, {nome:inputRemetente, mensagem:inputMensagem}])
+    setInputMensagem("")
+    setInputRemetente("")
+  }
+
+
+  const mensagensMapeada = primeiraMensagem.map((pessoa, index) => {
+    return(
+        <div key={index}>
+            <p>{pessoa.nome}</p>
+            <p>{pessoa.mensagem}</p>
+        </div>
+    )
+})
+
+
   return (
-    <main>
-      <input type='text' name='remetente' id='remetente'/>
-      <input type='text' name='mensagem' id='mensagem'/>
-      <button>Enviar</button>
-    </main>
+    <DivCentral>
+      
+      <MsgInicial>{mensagensMapeada}</MsgInicial>
+      
+      <label/>
+      <MsgInicial>
+      <input
+        placeholder='Remetente' 
+        id='remetente'
+        value={inputRemetente}
+        onChange={handleInputRemetente}
+      />
+      </MsgInicial>
+      <label/>
+      <MsgInicial>
+      <input
+        placeholder='Escreva sua mensagem'
+        id='mensagem'
+        value={inputMensagem}
+        onChange={handleInputMensagem}
+      />
+      </MsgInicial>
+      <button onClick={addMensagem}>Enviar</button>
+      
+    </DivCentral>
 
 
 
