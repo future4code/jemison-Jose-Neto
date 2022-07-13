74722f73e57beff63dd2f5d7c56bd384fd9b2fa2
jose neto <jnetocdz@hotmail.com.br>
2022-07-12 23:39:12 -0300

    exercicio-incompleto

---
+++ modulo3/rend-condicional-form/rend-condicional/src/components/Etapa1.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export function Etapa1() {
+
+
+return (
+    <div>
+        <h1>ETAPA 1 - DADOS GERAIS</h1>
+        <form>
+            <h2>1. Qual é seu nome?</h2>
+            <input placeholder="Nome"/>
+        </form>
+        <form>
+            <h2>2. Qual é sua idade?</h2>
+            <input placeholder="Idade"/>
+        </form>
+        <form>
+            <h2>3. Qual é seu e-mail?</h2>
+            <input placeholder="E-mail"/>
+        </form>
+        <h2>4. Qual é a sua escolaridade?</h2>
+
+
+    </div>
+);
+}
+
+export default Etapa1;
---
+++ modulo3/rend-condicional-form/rend-condicional/src/components/Final.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Final() {
+return (
+    <div>
+        <h1>O FORMULÁRIO ACABOU</h1>
+        <h2>Muito obrigado por participar! Entraremos em contato!</h2>
+        
+
+
+
+    </div>
+);
+}
+
+export default Final;
