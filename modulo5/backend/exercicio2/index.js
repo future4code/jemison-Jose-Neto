091b53c1b313ff10c89b564e12d33599c90ea571
jose neto <jnetocdz@hotmail.com.br>
2022-10-12 13:00:22 -0300

    rev JS

--- modulo5/backend/exercicio2/index.js
+++ modulo5/backend/exercicio2/index.js
@@ -23,28 +23,21 @@ console.log(addClient('zé'))
 
 const tabuada=(input)=>{
     
-    let resultado
+    let resultado = []
 
-    if (input = !Number){
-        resultado = `Erro. Parâmetro inválido (deve ser um número)`
-    } else if (input > 10){
-        resultado = 'Erro. Parâmetro inválido (número precisa valer entre 1 e 10)'
-    } else     
-        resultado =[
-        `${input} x 0 = ${input * 0}`,
-        `${input} x 1 = ${input * 1}`,
-        `${input} x 2 = ${input * 2}`,
-        `${input} x 3 = ${input * 3}`,
-        `${input} x 4 = ${input * 4}`,
-        `${input} x 5 = ${input * 5}`,
-        `${input} x 6 = ${input * 6}`,
-        `${input} x 7 = ${input * 7}`,
-        `${input} x 8 = ${input * 8}`,
-        `${input} x 9 = ${input * 9}`,
-        `${input} x 10 = ${input * 10}`,
-	]
+    if (typeof input !== "number"){
+        return `Erro. Parâmetro inválido (deve ser um número)`
+    }
+    if (input > 10){
+        return `Erro. Parâmetro inválido (número precisa valer entre 1 e 10)`
+    }
+    
+    for (let i = 0; i <= 10; i++){
+        resultado.push(`${input} x ${i} = ${input * i}`)
+    }
 
 return resultado
 }
 
-console.log(tabuada(20));
+
+console.log(tabuada(5));
--- modulo5/backend/exercicio3/index.js
+++ modulo5/backend/exercicio3/index.js
@@ -1,1 +1,31 @@
 console.log("exercicio 3");
+
+const contas = [
+	{
+		email: "astrodev@labenu.com",
+		password: "abc123"
+	},
+	{
+		email: "bananinha@gmail.com",
+		password: "bananinha"
+	}
+]
+
+const addConta=(usuario, senha)=>{
+
+    const novaConta = {
+        email: usuario,
+        password: senha
+        }
+
+    if ( typeof usuario !== 'e-mail' ) {
+        return console.log('');
+    }
+
+    
+    contas.push(novaConta)
+    return contas
+}
+
+
+console.log(addConta('abigegegegee', 'vrum'));
