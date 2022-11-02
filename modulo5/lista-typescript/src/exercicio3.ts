e6e2417f7bedb6e1711f462f23b87234f7724e41
jose neto <jnetocdz@hotmail.com.br>
2022-11-02 11:53:17 -0300

    lista

--- modulo5/lista-typescript/src/exercicio3.ts
+++ modulo5/lista-typescript/src/exercicio3.ts
@@ -1,19 +1,19 @@
-function filme (nome:string, anoLancamento:number, genero:enum, nota?:number ) {
+// function filme (nome:string, anoLancamento:number, genero:enum, nota?:number ) {
 
-    enum GENERO {
-        ACAO="ação",
-        DRAMA="drama",
-        COMEDIA="comédia",
-        ROMANCE="romance",
-        TERROR="terror"
-    }
+//     enum GENERO {
+//         ACAO="ação",
+//         DRAMA="drama",
+//         COMEDIA="comédia",
+//         ROMANCE="romance",
+//         TERROR="terror"
+//     }
 
-    type DadosFilme{
-        nome: string ,
-        anoLancamento: ,
-        genero: GENERO
-    }
+//     // type DadosFilme{
+//     //     nome: string ,
+//     //     anoLancamento: ,
+//     //     genero: GENERO
+//     // }
 
 
-    return 
-} 
+//     return 
+// } 
